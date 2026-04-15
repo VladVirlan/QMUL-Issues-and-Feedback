441d8ea06370fbe91530bb4b891162f599306b0b
Xinyu Gu <197852450+Xinyu-Gu736@users.noreply.github.com>
2026-04-15 19:16:26 +0100

    Added filter button

--- src/ticket_create_page/components/IntroPage.jsx
+++ src/ticket_create_page/components/IntroPage.jsx
@@ -5,6 +5,8 @@ import "./IntroPage.css";
 const IntroPage = ({setPage, setTicketType, setTicket}) => {
 
     const [tickets, setTickets] = useState([]);
+    const [userEmail, setUserEmail] = useState([]);
+    const [showAll, setShowAll] = useState(true);
 
     function displayTicketType(ticketType){
         if(ticketType == "it"){
@@ -24,24 +26,52 @@ const IntroPage = ({setPage, setTicketType, setTicket}) => {
             if(!error){
                 setTickets(data);
             }
+
+            const {data: {user}} = await supabase.auth.getUser();
+            setUserEmail(user?.email);
         }
         getTickets();
     }, []);
 
+    async function toggleFilter(){
+        if(showAll){
+            const {data, error} = await supabase.from('tickets').select('*').eq("student_email", userEmail).order('created_at', {ascending: false});
+            if(!error){
+                setTickets(data);
+                setShowAll(false);
+            }
+        }
+        else{
+            const {data, error} = await supabase.from('tickets').select('*').order('created_at', {ascending: false});
+            if(!error){
+                setTickets(data);
+                setShowAll(true);
+            }
+        }
+    }
+
     return (
         <div className="main_body">
+            <div className="ticket_intro_heading">
+                <h2 className="tickets_heading">Tickets</h2>
+                <button className="button" onClick={() => {toggleFilter()}}>
+                    {showAll ? "Show My Tickets" : "Show All Tickets"}
+                </button>
+            </div>
             <div className="display_tickets" >
-                {tickets.map((ticket) => (
-                    <button className="ticket" onClick={() => {setPage("ticket_details"); setTicket(ticket)}}>
-                        <div className="title_status">
-                            <h1>{ticket.title}</h1>
-                            <p className="ticket_type">{displayTicketType(ticket.type)}</p>
-                            <p className="ticket_p">{ticket.status}</p>
-                        </div>
-                        <p className="ticket_email">{ticket.student_email}</p>
-                        <p className="ticket_p">{ticket.message}</p>
-                    </button>
-                ))}
+                {
+                    tickets.map((ticket) => (
+                        <button className="ticket" onClick={() => {setPage("ticket_details"); setTicket(ticket)}}>
+                            <div className="title_status">
+                                <h1>{ticket.title}</h1>
+                                <p className="ticket_type">{displayTicketType(ticket.type)}</p>
+                                <p className="ticket_p">{ticket.status}</p>
+                            </div>
+                            <p className="ticket_email">{ticket.student_email}</p>
+                            <p className="ticket_p">{ticket.message}</p>
+                        </button>
+                    ))
+                }
             </div>
             <div className="submit_tickets">
                 <p>Want to report an issue? Submit a ticket</p>
